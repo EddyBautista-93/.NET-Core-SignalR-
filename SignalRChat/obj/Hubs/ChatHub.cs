4c6d7b857a16df75aba0a611875e9cdbf9439d88
EddyBautista <bautista.j.eddy@gmail.com>
2020-08-10 05:13:08 -0500

    added endpoint and service for singalR

--- SignalRChat/obj/Hubs/ChatHub.cs
+++ SignalRChat/obj/Hubs/ChatHub.cs
@@ -3,8 +3,14 @@ using System.Threading.Tasks;
 
 namespace SignalRChat.Hubs
 {
+    // A Hub is a class that serves as a high-level pupline that handles client-server communications 
+    //The ChatHub Class inherits from the SignalR HubClass , this manages the connection, groups, and messaging.
     public class ChatHub : Hub 
     {
+        //The SendMessage method can be called by a connected client to send a message to call clients, 
+        //Javascript client code call the method that is shown later. 
+        // Asynchronous to provide maximum scalability.
+
         public async Task SendMessage(string user, string message)
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
